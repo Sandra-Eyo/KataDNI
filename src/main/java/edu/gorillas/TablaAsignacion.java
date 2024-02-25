a0d1a0f4414799400a40e7c8967df55cf71f4c4d
Sandra-Eyo <seyo.smr@gmail.com>
2024-02-25 19:22:18 +0100

    Añadidos métodos de TablaAsignacion

--- src/main/java/edu/gorillas/TablaAsignacion.java
+++ src/main/java/edu/gorillas/TablaAsignacion.java
@@ -7,6 +7,23 @@ public class TablaAsignacion {
                                               'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
 
 
+    public char getLetra(int indice) {
+        return LETRAS_ASIGNACION[indice];
+    }
 
+    public boolean letraPermitida(char letra) {
+        char[] letrasNoPermitidas = {'I', 'Ñ', 'O', 'U'};
+        for (char noPermitida : letrasNoPermitidas) {
+            if (letra == noPermitida) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public char calcularLetra(String parteNumericaDni) {
+        int indice = Integer.parseInt(parteNumericaDni) % 23;
+        return getLetra(indice);
+    }
 
 }
